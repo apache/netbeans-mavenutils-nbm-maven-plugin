47b49345a67ed23df0bde5271c4a4104f68595bd
Eric Barboni <skygo@apache.org>
2019-05-15 11:45:04 +0200

    fix ASF header, fix author

--- src/main/java/org/apache/netbeans/nbm/CreateStandaloneMojo.java
+++ src/main/java/org/apache/netbeans/nbm/CreateStandaloneMojo.java
@@ -1,19 +1,22 @@
 /*
- *  Copyright 2008 Johan Andrén.
- * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- * 
- *       http://www.apache.org/licenses/LICENSE-2.0
- * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *  under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package org.apache.netbeans.nbm;
 
 import java.io.File;
@@ -29,7 +32,7 @@ import org.codehaus.plexus.archiver.zip.ZipArchiver;
 /**
  * Create a standalone application out of the composed clusters of nbm-application
  *
- * @author <a href="mailto:johan.andren@databyran.se">Johan Andrén</a>
+ * @author Johan Andrén
  * @author Milos Kleint
  */
 @Mojo(name="standalone-zip", requiresProject=true, threadSafe = true)
